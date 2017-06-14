1a8a5c6d2b8f4d1195943ce532ab6e693e69380a
vanillin <vanillin97@gmail.com>
2017-06-14 21:49:58 +0300

    pz2

---
+++ src/ua/lb/pz2/text400.java
@@ -0,0 +1,60 @@
+package ua.lb.pz2;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by vanillin on 14.06.2017.
+ */
+public class text400 {
+    public static void main(String[] args) {
+
+        String s = "Abstract. The Kalyna block cipher was selected during Ukrainian National Public Cryptographic Competition (2007-2010) and its slight modifcation was approved as the new encryption standard of Ukraine in 2015.Main requirements for Kalyna were both high security level and high per-\n" +
+                "formance of software implementation on general-purpose 64-bit CPUs.\n" +
+                "The cipher has SPN-based (Rijndael-like) structure with increased MDS\n" +
+                "matrix size, a new set of four di\u000Berent S-boxes, pre- and postwhitening\n" +
+                "using modulo 264 addition and a new construction of the key schedule.\n" +
+                "Kalyna supports block size and key length of 128, 256 and 512 bits (key\n" +
+                "length can be either equal or double of the block size). On the time of\n" +
+                "this paper publishing, no more e\u000Bective cryptanalytic attacks than ex-\n" +
+                "haustive search are known. In this paper we present the adapted English\n" +
+                "translated speci\fcation of Kalyna as it is given in the national standard\n" +
+                "of Ukraine.";
+
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.get(s.charAt(i)) != null) {
+                int n = hm.get(s.charAt(i));
+                hm.put(s.charAt(i), ++n);
+                System.out.println((s.charAt(i)) + " " + n);
+            } else {
+                hm.put(s.charAt(i), 1);
+                System.out.println((s.charAt(i)));
+            }
+        }
+
+
+       /* Map<Character, Integer> result = new LinkedHashMap<>();
+
+        hm.entrySet().stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .forEachOrdered(x -> result.put(x.getKey(), x.getValue()));
+
+        System.out.println(result);*/
+
+       /* Pattern pattern = Pattern.compile("м");
+        Matcher matcher = pattern.matcher(s);
+        int n = 0;
+        while (matcher.find()) {
+            //System.out.println(matcher.group());
+            n++;
+        }
+        System.out.println("N = " + n);*/
+
+    }
+}
